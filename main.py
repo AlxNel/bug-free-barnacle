f3fc38058f216a0c3c9905cf77644b7591cde57e
Александр Нелюбин <alxnel@mail.ru>
2023-01-03 01:10:00 +0300

    обновленная версия

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('Hello, world')
+import math
+
+FIGURES = [
+    {'type':'round','params':[3]},
+    {'type':'rectangle','params':[3,4]},
+    {'type':'rectangle','params':[4,10]},
+    {'type':'round','params':[9]},
+]
+
+def get_square(figure):
+    if figure['type'] == 'round':
+        return math.pi * figure['params'][0] * figure['params'][0]
+    if figure['type'] == 'rectangle':
+        return figure['params'] [0] *figure['params'][1]
+
+print(
+    sum(
+        map(
+            get_square,
+            FIGURES
+        )
+    )
+)
